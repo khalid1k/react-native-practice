c4faa45bb4a7cf0a00cdffbb0060d8a2ba7372f4
Muhammad Khalid <khalidanmol339@gmail.com>
2025-07-17 11:06:47 +0500

    first commit

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { View, Text, Button } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text style= {{fontSize: 40}}>helo world</Text>
+      <Text>i am khalid</Text>
+      <Button title='Tab here'/>
+    </View>
+  )
+}
+
+export default App
